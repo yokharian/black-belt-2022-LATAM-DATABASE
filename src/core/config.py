1af8af7a8971dcda7bcc713df82ad49d3af0fde5
Sofia Escobedo <cdmx.sofia@gmail.com>
2022-11-10 17:42:59 -0600

    removed serverless implementation

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,9 @@
-import sys
 from pathlib import Path
-from typing import Optional
-from os import getenv
+from os import environ, getenv
 
-from loguru import logger
 from starlette.middleware.cors import ALL_METHODS
 
 STAGE = getenv("STAGE", "dev").lower()
-PROJECT_NAME = f"REST-API microservice {STAGE}".upper()
 
 HEADERS = {
     "Access-Control-Allow-Origin": "*",
@@ -16,24 +12,6 @@ HEADERS = {
     "Access-Control-Allow-Credentials": "true",
 }
 
-AWS_DEFAULT_REGION: str = getenv("AWS_DEFAULT_REGION", "us-east-1")
-LOCALLY_DEBUG: bool = bool(int(getenv("IN_UVICORN", "0")))
-
-# region LOG CONFIGS
-LOGURU_LEVEL: Optional[str] = getenv("LOGURU_LEVEL")
-if not LOGURU_LEVEL:
-    LOGURU_LEVEL: str = "DEBUG" if STAGE == "dev" else "INFO"
-# reset level
-logger.remove()
-logger.add(sys.stdout, level=LOGURU_LEVEL)
-
-DEV_LOG_FORMAT = """<green>{time:HH:mm:ss}</green> | \
-<cyan>{level}</cyan> | <level>{message}</level>"""
-PROD_LOG_FORMAT = """{level} | <level>{message}</level>"""
-LOGURU_FORMAT = PROD_LOG_FORMAT if STAGE != "dev" else DEV_LOG_FORMAT
-# endregion
-
-
+environ['AWS_DEFAULT_REGION'] = getenv("AWS_DEFAULT_REGION") or 'us-east-1'
 HERE = Path(__file__).parent
 ROOT = HERE.parent.parent
-STATIC = ROOT.joinpath("static")
--- src/core/utils.py
+++ src/core/utils.py
@@ -1,14 +1,5 @@
 import json
 
-# import yaml
-
-
-def load_yaml(file_path):
-    """Load a yaml file."""
-    with open(file_path, "r") as file:
-        return yaml.safe_load(file)
-
-
 def load_json(file_path):
     """Load a json file."""
     with open(file_path, "r") as file:
